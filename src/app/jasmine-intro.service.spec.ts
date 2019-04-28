a665cea0b6b02efe1a7eb874e0fb3641a9148e95
chadyeh <chadyeh.yc@outlook.com>
2019-04-28 11:25:16 +0800

    Modify  將Service 移到 beforeEach New

--- src/app/jasmine-intro.service.spec.ts
+++ src/app/jasmine-intro.service.spec.ts
@@ -1,42 +1,35 @@
 import { JasmineIntroService } from './jasmine-intro.service';
 
 describe('JasmineIntroService', () => {
+  let service: JasmineIntroService;
   beforeAll(() => {
     console.log('beforeAll');
   });
 
   beforeEach(() => {
     console.log('beforeEach');
-    const service: JasmineIntroService = new JasmineIntroService();
+    service = new JasmineIntroService();
   });
 
   it('should add two numbers', () => {
-    const service: JasmineIntroService = new JasmineIntroService();
-
-    const result = service.addNumber(1, 2);
+        const result = service.addNumber(1, 2);
 
     expect(result).toEqual(3);
   });
 
   it('should subtract two numbers', () => {
-    const service: JasmineIntroService = new JasmineIntroService();
-
     const result = service.subtractNumber(1, 2);
 
     expect(result).toEqual(-1);
   });
 
   it('should multiplication two number', () => {
-    const service: JasmineIntroService = new JasmineIntroService();
-
     const result = service.multiplicationNumber(4, 2);
 
     expect(result).toEqual(8);
   });
 
   it('should division two number', () => {
-    const service: JasmineIntroService = new JasmineIntroService();
-
     const result = service.divisionNumber(4, 2);
 
     expect(result).toEqual(2);
