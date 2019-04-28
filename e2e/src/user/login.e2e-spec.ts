4573e43d8073d454df9afe5381100f88d6d0b711
chadyeh <chadyeh.yc@outlook.com>
2019-04-28 12:14:50 +0800

    Modify 新增登入成功與失敗的測試

--- e2e/src/user/login.e2e-spec.ts
+++ e2e/src/user/login.e2e-spec.ts
@@ -1,3 +1,29 @@
-describe('the user try to login', () => {
+import { browser, by, element } from 'protractor';
 
+describe('the user try to login', () => {
+  it('should login to event page', () => {
+    browser.get('/user/login');
+    element(by.id('userName')).sendKeys('John');
+    element(by.id('password')).sendKeys('123456');
+    element(by.buttonText('登入')).click();
+    const actual = browser.getCurrentUrl();
+    expect(actual).toContain('events');
+  });
+
+  it('should login failure alert', () => {
+    browser.get('/user/login');
+    element(by.id('userName')).sendKeys('John');
+    element(by.id('password')).sendKeys('654321');
+    element(by.buttonText('登入')).click();
+    expect(element(by.css('div.alert.alert-danger')).getText()).toContain('錯誤的帳號密碼');
+  });
+
+  it('should login failure alert is present', () => {
+    browser.get('/user/login');
+    element(by.id('userName')).sendKeys('John');
+    element(by.id('password')).sendKeys('654321');
+    element(by.buttonText('登入')).click();
+    const isAlertPresent = element(by.className('alert-danger')).isPresent();
+    expect(isAlertPresent).toBe(true, '錯誤密碼案例驗證失敗');
+  });
 });
